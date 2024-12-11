99d1996465e0765e332ccf38864846cc3075474c
HyunhLee <lhh940727@gmail.com>
2024-12-11 15:34:36 +0900

    2일차

---
+++ src/defaultPakage/StringApp.java
@@ -0,0 +1,16 @@
+package defaultPakage;
+
+public class StringApp {
+    public static void main(String[] args) {
+        System.out.println("Hello World");
+        System.out.println('H');
+        System.out.println("H");
+
+        System.out.println("Hello "
+                + "world");
+
+        System.out.println("Hello \nWorld");
+
+        System.out.println("Hello \"World\"");
+    }
+}
